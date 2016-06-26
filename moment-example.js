10bc219143402d3125fc0b1c19bc6eaacbb9ed3a
Ilan Ben Tal <ilandbt@gmail.com>
2016-06-26 22:54:02 +0300

    Added moment example

---
+++ moment-example.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+console.log(now.format(X));
+
+// console.log(now.format('MMM Do YYYY, h:mma'));// Oct 5th 2016, 12:34pm
